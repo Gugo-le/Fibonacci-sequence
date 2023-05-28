a8d063d803745584ef361ed73e933cf8fcf81048
Gugo-le <rngustmd0719@naver.com>
2023-05-29 00:18:53 +0900

    iter

---
+++ iter_fibo.py
@@ -0,0 +1,19 @@
+import time
+
+def iter_fibonacci(n):
+    a, b = 1, 1
+    if n < 2:
+        return 1
+    else:
+        for i in range(n-2):
+            c = a + b
+            a, b = b, c
+        return c   
+  
+start_time = time.time()
+
+print(iter_fibonacci(40))
+
+elapsed_time = time.time() - start_time
+
+print("총 소요시간: {}초".format(elapsed_time))
