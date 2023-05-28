1d1bec4c2485c7a15711cbd7928676f9492f5393
Gugo-le <rngustmd0719@naver.com>
2023-05-29 00:17:48 +0900

    recursive

---
+++ recursive_fibo.py
@@ -0,0 +1,15 @@
+import time
+
+def recursive_fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
+
+start_time = time.time()
+
+print(recursive_fibonacci(40))
+
+elapsed_time = time.time() - start_time
+
+print("총 소요시간: {}초".format(elapsed_time))
