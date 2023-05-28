5d1d36e895a770721cc4b40e7fdb4b1ff2cda151
Gugo-le <rngustmd0719@naver.com>
2023-05-29 00:21:21 +0900

    equation

---
+++ equation_fibo.py
@@ -0,0 +1,13 @@
+import time
+
+def equation_fibonacci(n):
+    root_5 = 5 ** (1/2)
+    return (1 / root_5) * ( ( (1 + root_5) / 2)**n - ( (1 - root_5) / 2)**n )
+
+start_time = time.time()
+
+print(equation_fibonacci(40))
+
+elapsed_time = time.time() - start_time
+
+print("총 소요시간: {}초".format(elapsed_time))
